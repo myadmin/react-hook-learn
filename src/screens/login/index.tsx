d30a1e8a8f6f934ce61f503efa1116bd6cedf7eb
zhouwei <zhou21192345@163.com>
2021-02-24 22:56:50 +0800

    feat: 封装基本的登录注册接口

--- src/screens/login/index.tsx
+++ src/screens/login/index.tsx
@@ -1,33 +1,26 @@
 import React, { FormEvent } from "react";
-
-const apiUrl = process.env.REACT_APP_API_URL;
+import { useAuth } from "../../context/auth-context";
 
 export const LoginScreen = () => {
-    const login = (param: { username: string; password: string }) => {
-        fetch(`${apiUrl}/login`, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify(param),
-        }).then(async (response) => {
-            if (response.ok) {
-            }
-        });
-    };
+    const { login, user } = useAuth();
 
-    const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+    const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
         event.preventDefault();
         const username = (event.currentTarget.elements[0] as HTMLFormElement)
             .value;
         const password = (event.currentTarget.elements[1] as HTMLFormElement)
             .value;
 
-        login({ username, password });
+        await login({ username, password });
     };
 
     return (
         <form onSubmit={handleSubmit}>
+            {user ? (
+                <p>
+                    登录成功，用户名：{user?.name} token{user.token}
+                </p>
+            ) : null}
             <div>
                 <label htmlFor="username">用户名</label>
                 <input type="text" id={"username"} />
