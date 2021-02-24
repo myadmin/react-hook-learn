48e3810f03d1933b90e7ec57894e26f9df1f16f9
zhouwei <zhou21192345@163.com>
2021-02-24 18:32:31 +0800

    feat: 添加工程列表

---
+++ src/test.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+interface PropInterface {}
+
+interface StateInterface {}
+
+class Demo extends React.Component<PropInterface, StateInterface> {
+  constructor(props: PropInterface) {
+    super(props);
+  }
+}
