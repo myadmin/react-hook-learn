c2e9f5bc9e135a93b0d7e9ea6d1bb9e742ab1a4a
zhouwei <zhou21192345@163.com>
2021-02-24 20:00:11 +0800

    feat: 添加 useMount 和 useDebounce 两个hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import React from "react";
-import { ProjectListScreen } from "./screens/project-list";
+import { ProjectListScreen } from "./screens/project-list/index";
 import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <ProjectListScreen />
-    </div>
-  );
+    return (
+        <div className="App">
+            <ProjectListScreen />
+        </div>
+    );
 }
 
 export default App;
--- src/screens/project-list/index.jsx
+++ src/screens/project-list/index.jsx
@@ -1,40 +1,43 @@
 import React, { useEffect, useState } from "react";
-import { SearchPanel } from './search-panel';
-import { List } from './list';
-import { cleanObject } from "../../utils";
-import qs from 'qs'
+import { SearchPanel } from "./search-panel";
+import { List } from "./list";
+import { cleanObject, useDebounce, useMount } from "../../utils";
+import qs from "qs";
 
 const apiUrl = process.env.REACT_APP_API_URL;
 
 export const ProjectListScreen = () => {
-  const [users, setUsers] = useState([]);
+    const [users, setUsers] = useState([]);
 
-  const [param, setParam] = useState({
-    name: '',
-    personId: ''
-  });
-  const [list, setList] = useState([]);
-
-  useEffect(() => {
-    fetch(`${apiUrl}/projects?${qs.stringify(cleanObject(param))}`).then(async response => {
-      if (response.ok) {
-        setList(await response.json())
-      }
+    const [param, setParam] = useState({
+        name: "",
+        personId: "",
     });
-  }, [param]);
+    const debouncedState = useDebounce(param, 200);
+    const [list, setList] = useState([]);
 
-  useEffect(() => {
-    fetch(`${apiUrl}/users`).then(async response => {
-      if (response.ok) {
-        setUsers(await response.json())
-      }
+    useEffect(() => {
+        fetch(`${apiUrl}/projects?${qs.stringify(cleanObject(param))}`).then(
+            async (response) => {
+                if (response.ok) {
+                    setList(await response.json());
+                }
+            }
+        );
+    }, [debouncedState]);
+
+    useMount(() => {
+        fetch(`${apiUrl}/users`).then(async (response) => {
+            if (response.ok) {
+                setUsers(await response.json());
+            }
+        });
     });
-  }, []);
 
-  return (
-    <div>
-      <SearchPanel param={param} users={users} setParam={setParam}/>
-      <List users={users} list={list}/>
-    </div>
-  )
+    return (
+        <div>
+            <SearchPanel param={param} users={users} setParam={setParam} />
+            <List users={users} list={list} />
+        </div>
+    );
 };
