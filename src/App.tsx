2dad2d492a223e277fd6277392cc06df77d0a59b
zhouwei <zhou21192345@163.com>
2021-02-24 22:11:47 +0800

    feat: 模拟登录

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import React from "react";
-import { ProjectListScreen } from "./screens/project-list";
+// import { ProjectListScreen } from "./screens/project-list";
 import "./App.css";
-import { TsReactTest } from "./try-use-array";
+import { LoginScreen } from "./screens/login";
+// import { TsReactTest } from "./try-use-array";
 
 function App() {
     return (
         <div className="App">
+            <LoginScreen />
             {/*<ProjectListScreen />*/}
-            <TsReactTest />
+            {/*<TsReactTest />*/}
         </div>
     );
 }
