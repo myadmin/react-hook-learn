8aedd00af2973f7ab5148c655ff0851651078acb
zhouwei <zhou21192345@163.com>
2021-02-25 17:07:56 +0800

    feat: 学习TS的类型别名相关内容

--- src/utils/http.ts
+++ src/utils/http.ts
@@ -47,6 +47,38 @@ export const http = async (
 
 export const useHttp = () => {
     const { user } = useAuth();
+    //utility type 的用法：用泛型给它传入一个其它类型，然后utility type对这个类型进行某种操作
     return (...[endpoint, config]: Parameters<typeof http>) =>
         http(endpoint, { ...config, token: user?.token });
 };
+
+//联合类型
+// let myFavoriteNumber: string | number;
+// myFavoriteNumber = "seven";
+// myFavoriteNumber = 7;
+// // TS2322: Type '{}' is not assignable to type 'string | number'.
+// // myFavoriteNumber = {}
+// let jackFavoriteNumber: string | number;
+//
+
+// //类型别名，interface 在这种情况下没法替代type
+// type FavoriteNumber = string | number;
+// let roseFavoriteNumber: FavoriteNumber = 6;
+
+// interface 也没法实现Utility type
+// type Person = {
+//     name: string,
+//     age: number
+// };
+
+// const xiaoMing: Partial<Person> = {age: 8};
+// const shenMiRen: Omit<Person, 'name'> = {age: 9};
+// type PersonKeys = keyof Person;
+// type PersonKeys = "name" | "age";
+// type PersonOnlyName = Pick<Person, 'name'>;
+// type Age = Exclude<PersonKeys, 'name'>;
+
+//Partial 的实现
+// type Partial<T> = {
+//     [P in keyof T]?: T[P]
+// }
